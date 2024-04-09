6cf3e7b57b25eaa80a67df286f24c6bb3592d08d
TungNguyenAnh <114374972+TungNguyenAnh@users.noreply.github.com>
2024-04-09 19:55:06 +0700

    Create TimTrongMang.cs

---
+++ TimTrongMang.cs
@@ -0,0 +1,18 @@
+﻿string[] myStudent = { "Mai", "An", "Hao", "Hoang" };
+System.Console.WriteLine("Nhap ten");
+string name = Console.ReadLine();
+
+bool availble = false;
+for (int i = 0; i < myStudent.Length; i++)
+{
+    if (myStudent[i].Equals(name))
+    {
+        System.Console.WriteLine("Co trong lop");
+        availble = true;
+        break;
+    }
+    if (!availble)
+    {
+        System.Console.WriteLine("Khong co trong lop");
+    }
+}
